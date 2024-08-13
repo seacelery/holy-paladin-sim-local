9f5b227d10f66a75952fbf1ebb0787bf58208fb0
Amelia <seaceleries@gmail.com>
2024-08-13 19:46:42 +0100

    update supported trinkets list

--- frontend/src/js/components/equipment-options.js
+++ frontend/src/js/components/equipment-options.js
@@ -828,7 +828,7 @@ const initialiseEquipment = () => {
             "Eye of the Broodmother", "Emerald Coach's Whistle", "Unbound Changeling", "Gruesome Syringe",
             "Harvester's Edict", "Scrapsinger's Symphony", "High Speaker's Accretion", "Ovinax's Mercurial Egg",
             "Empowering Crystal of Anub'ikkaj", "Siphoning Phylactery Shard", "Creeping Coagulum", "Treacherous Transmitter",
-            "Mereldar's Toll"
+            "Mereldar's Toll", "Ara-Kara Sacbrood", "Gale of Shadows", "Corrupted Egg Shell", "Spymaster's Web"
         ];
         if ((currentItemSlot === "trinket_1" || currentItemSlot === "trinket_2") && !supportedTrinkets.includes(itemName)) {
             trinketUnsupported.style.display = "block";
